2e37899a6258a224df2bd7060028df0bb692fd27
rin-windows <phurinmeng@gmail.com>
2025-03-17 20:37:57 -0700

    Add particle effects for brick destruction and update prefab references

--- Assets/_Scripts/Logic/Brick.cs
+++ Assets/_Scripts/Logic/Brick.cs
@@ -5,6 +5,8 @@ public class Brick : MonoBehaviour
 {
     private Coroutine destroyRoutine = null;
 
+    public GameObject vfx_BrickExplosion;
+
     private void OnCollisionEnter(Collision other)
     {
         if (destroyRoutine != null) return;
@@ -17,5 +19,9 @@ public class Brick : MonoBehaviour
         yield return new WaitForSeconds(0.1f); // two physics frames to ensure proper collision
         GameManager.Instance.OnBrickDestroyed(transform.position);
         Destroy(gameObject);
+        // instantiate the particle effect at the brick's position
+        GameObject particleInstance = Instantiate(vfx_BrickExplosion, transform.position, Quaternion.identity);
+        // then destroy the particle instance after 1 second
+        Destroy(particleInstance, 1f);
     }
 }
